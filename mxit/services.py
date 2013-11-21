91a84883bfbf3f41e138fae945cd2ea0f2d9d5bb
Philip Perold <prperold@gmail.com>
2013-11-21 13:51:19 +0200

    user methods

--- mxit/services.py
+++ mxit/services.py
@@ -1,4 +1,5 @@
 import json
+import urllib
 from requests import get, post
 from mxit import settings
 from mxit.exceptions import MxitAPIException
@@ -27,9 +28,74 @@ class MessagingService(BaseService):
             }
         )
 
+    def send_user_to_user_message(self, from_user_id, target_user_ids, message='', contains_markup=True):
+        """
+        Send a message (from a Mxit app) to a list of Mxit users
+        """
+        print('Token: ' + self.oauth.get_user_token('message/user'))
+        return _post(
+            token=self.oauth.get_user_token('message/user'),
+            uri='/message/send/',
+            data={
+                'From': from_user_id,
+                'To': ",".join(target_user_ids),
+                'Body': message,
+                'ContainsMarkup': contains_markup
+            }
+        )
+
 
 class UserService(BaseService):
-    pass
+
+    def user_id(self, mxit_id):
+        user_id = _get(
+            token=self.oauth.get_app_token('profile/public'),
+            uri='/user/lookup/' + urllib.quote(mxit_id)
+        )
+
+        if user_id.startswith('"') and user_id.endswith('"'):
+            user_id = user_id[1:-1]
+
+        return user_id
+
+    def status(self, mxit_id):
+        status = _get(
+            token=self.oauth.get_app_token('profile/public'),
+            uri='/user/public/statusmessage/' + urllib.quote(mxit_id)
+        )
+
+        if status.startswith('"') and status.endswith('"'):
+            status = status[1:-1]
+
+        return status
+
+    def display_name(self, mxit_id):
+        display_name = _get(
+            token=self.oauth.get_app_token('profile/public'),
+            uri='/user/public/displayname/' + urllib.quote(mxit_id)
+        )
+
+        if display_name.startswith('"') and display_name.endswith('"'):
+            display_name = display_name[1:-1]
+
+        return display_name
+
+    def avatar(self, mxit_id):
+        return _get(
+            token=self.oauth.get_app_token('profile/public'),
+            uri='/user/public/avatar/' + urllib.quote(mxit_id)
+        )
+
+    def basic_profile(self, user_id):
+        profile = _get(
+            token=self.oauth.get_app_token('profile/public'),
+            uri='/user/profile/' + urllib.quote(user_id)
+        )
+
+        try:
+            return json.loads(profile)
+        except:
+            raise MxitAPIException('Error parsing profile data')
 
 
 # HTTP helper methods
