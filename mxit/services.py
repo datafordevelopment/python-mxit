23d02b5bbb024d760ab9d57d5074bb576ad47120
Philip Perold <prperold@gmail.com>
2013-11-22 09:44:25 +0200

    contact list

--- mxit/services.py
+++ mxit/services.py
@@ -12,13 +12,13 @@ class BaseService():
 
 class MessagingService(BaseService):
 
-    def send_message(self, app_mxit_id, target_user_ids, message='', contains_markup=True):
+    def send_message(self, app_mxit_id, target_user_ids, message='', contains_markup=True, scope='message/send'):
         """
         Send a message (from a Mxit app) to a list of Mxit users
         """
 
         return _post(
-            token=self.oauth.get_app_token('message/send'),
+            token=self.oauth.get_app_token(scope),
             uri='/message/send/',
             data={
                 'From': app_mxit_id,
@@ -28,13 +28,12 @@ class MessagingService(BaseService):
             }
         )
 
-    def send_user_to_user_message(self, from_user_id, target_user_ids, message='', contains_markup=True):
+    def send_user_to_user_message(self, from_user_id, target_user_ids, message='', contains_markup=True, scope='message/user'):
         """
         Send a message (from a Mxit app) to a list of Mxit users
         """
-        print('Token: ' + self.oauth.get_user_token('message/user'))
         return _post(
-            token=self.oauth.get_user_token('message/user'),
+            token=self.oauth.get_user_token(scope),
             uri='/message/send/',
             data={
                 'From': from_user_id,
@@ -47,13 +46,13 @@ class MessagingService(BaseService):
 
 class UserService(BaseService):
 
-    def get_user_id(self, mxit_id):
+    def get_user_id(self, mxit_id, scope='profile/public'):
         """
         Retrieve the Mxit user's internal "user ID"
         No user authentication required
         """
         user_id = _get(
-            token=self.oauth.get_app_token('profile/public'),
+            token=self.oauth.get_app_token(scope),
             uri='/user/lookup/' + urllib.quote(mxit_id)
         )
 
@@ -62,13 +61,13 @@ class UserService(BaseService):
 
         return user_id
 
-    def get_status(self, mxit_id):
+    def get_status(self, mxit_id, scope='profile/public'):
         """
         Retrieve the Mxit user's current status
         No user authentication required
         """
         status = _get(
-            token=self.oauth.get_app_token('profile/public'),
+            token=self.oauth.get_app_token(scope),
             uri='/user/public/statusmessage/' + urllib.quote(mxit_id)
         )
 
@@ -77,24 +76,24 @@ class UserService(BaseService):
 
         return status
 
-    def set_status(self, message):
+    def set_status(self, message, scope='status/write'):
         """
         Set the Mxit user's status
         User authentication required with the following scope: 'status/write'
         """
         return _put(
-            token=self.oauth.get_user_token('status/write'),
+            token=self.oauth.get_user_token(scope),
             uri='/user/statusmessage',
             data=json.dumps(message)
         )
 
-    def get_display_name(self, mxit_id):
+    def get_display_name(self, mxit_id, scope='profile/public'):
         """
         Retrieve the Mxit user's display name
         No user authentication required
         """
         display_name = _get(
-            token=self.oauth.get_app_token('profile/public'),
+            token=self.oauth.get_app_token(scope),
             uri='/user/public/displayname/' + urllib.quote(mxit_id)
         )
 
@@ -103,13 +102,13 @@ class UserService(BaseService):
 
         return display_name
 
-    def get_avatar(self, mxit_id):
+    def get_avatar(self, mxit_id, scope='profile/public'):
         """
         Retrieve the Mxit user's avatar
         No user authentication required
         """
         return _get(
-            token=self.oauth.get_app_token('profile/public'),
+            token=self.oauth.get_app_token(scope),
             uri='/user/public/avatar/' + urllib.quote(mxit_id)
         )
 
@@ -120,20 +119,20 @@ class UserService(BaseService):
         """
         raise NotImplementedError()
 
-    def delete_avatar(self):
+    def delete_avatar(self, scope='avatar/write'):
         """
         Delete the Mxit user's avatar
         User authentication required with the following scope: 'avatar/write'
         """
         raise NotImplementedError()
 
-    def get_basic_profile(self, user_id):
+    def get_basic_profile(self, user_id, scope='profile/public'):
         """
         Retrieve the Mxit user's basic profile
         No user authentication required
         """
         profile = _get(
-            token=self.oauth.get_app_token('profile/public'),
+            token=self.oauth.get_app_token(scope),
             uri='/user/profile/' + urllib.quote(user_id)
         )
 
@@ -142,13 +141,13 @@ class UserService(BaseService):
         except:
             raise MxitAPIException('Error parsing profile data')
 
-    def get_full_profile(self):
+    def get_full_profile(self, scope='profile/private'):
         """
         Retrieve the Mxit user's full profile
         User authentication required with the following scope: 'profile/private'
         """
         profile = _get(
-            token=self.oauth.get_user_token('profile/public'),
+            token=self.oauth.get_user_token(scope),
             uri='/user/profile'
         )
 
@@ -157,34 +156,50 @@ class UserService(BaseService):
         except:
             raise MxitAPIException('Error parsing profile data')
 
-    def update_profile(self, data):
+    def update_profile(self, data, scope='profile/write'):
         """
         Update the Mxit user's profile
         User authentication required with the following scope: 'profile/write'
         """
         raise NotImplementedError()
 
-    def add_contact(self, user_id):
+    def add_contact(self, user_id, scope='contact/invite'):
         """
-        Retrieve the Mxit user's full profile
+        Add a contact
         User authentication required with the following scope: 'contact/invite'
         """
         raise NotImplementedError()
 
-    def get_contact_list(self, list_filter, skip=0, count=0):
+    def get_contact_list(self, list_filter, skip=None, count=None, scope='graph/read'):
         """
-        Retrieve the Mxit user's full profile
+        Retrieve the Mxit user's full contact list
         User authentication required with the following scope: 'graph/read'
         """
-        raise NotImplementedError()
+        params = {
+            'filter': list_filter
+        }
+        if skip:
+            params['skip'] = skip
+        if count:
+            params['count'] = count
 
-    def get_friend_suggestions(self):
+        contact_list = _get(
+            token=self.oauth.get_user_token(scope),
+            uri='/user/socialgraph/contactlist?' + urllib.urlencode(params)
+        )
+
+        try:
+            return json.loads(contact_list)
+        except:
+            raise MxitAPIException('Error parsing contact_list data')
+
+    def get_friend_suggestions(self, scope='graph/read'):
         """
         Retrieve the Mxit user's full profile
         User authentication required with the following scope: 'graph/read'
         """
         suggestions = _get(
-            token=self.oauth.get_user_token('graph/read'),
+            token=self.oauth.get_user_token(scope),
             uri='/user/socialgraph/suggestions'
         )
 
@@ -194,7 +209,20 @@ class UserService(BaseService):
             raise MxitAPIException('Error parsing suggestions data')
 
 
-# HTTP helper methods
+# Helpers
+
+CONTACT_LIST_FILTER = {
+    'all':          '@All',
+    'friends':      '@Friends',
+    'apps':         '@Apps',
+    'invites':      '@Invites',
+    'connections':  '@Connections',
+    'rejected':     '@Rejected',
+    'pending':      '@Pending',
+    'deleted':      '@Deleted',
+    'blocked':      '@Blocked',
+}
+
 
 def _get(token, uri, content_type='application/json'):
     headers = {
