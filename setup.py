7b5f9a3aab44af44e13954b80b849f8ea92a89d2
Philip Perold <prperold@gmail.com>
2013-11-29 15:52:59 +0200

    bump version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='mxit',
-    version='0.0.2',
+    version='0.0.3',
     author='Donovan Schönknecht',
     author_email='don@tpyo.net',
     packages=['mxit'],
