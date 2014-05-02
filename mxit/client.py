1a328e549fc4e016f1d78fe6a88885ff7b00411a
Graeme Glass <graemeglass@gmail.com>
2014-05-02 09:46:10 +0200

    for internal API calls we need a different OAuth provider

--- mxit/client.py
+++ mxit/client.py
@@ -7,10 +7,12 @@ class Mxit(object):
     Mxit API wrapper
     """
 
-    def __init__(self, client_id, client_secret, redirect_uri=None, state=None, cache=None, verify_cert=True):
-
+    def __init__(self, client_id, client_secret, redirect_uri=None, state=None, cache=None, verify_cert=True, OAuth_Provider=None):
         # Auth
-        self.oauth = OAuth(client_id, client_secret, redirect_uri, state, cache, verify_cert)
+        if OAuth_Provider:
+            self.oauth = OAuth_Provider(client_id, client_secret, redirect_uri, state, cache, verify_cert)
+        else:
+            self.oauth = OAuth(client_id, client_secret, redirect_uri, state, cache, verify_cert)
 
         # Services
         self.messaging = MessagingService(self.oauth)
