1644242b72e375837a893e83e66e4560652c75d3
Graeme Glass <graemeglass@gmail.com>
2014-05-05 16:28:44 +0200

    user_id for internal calls.

--- mxit/client.py
+++ mxit/client.py
@@ -7,10 +7,10 @@ class Mxit(object):
     Mxit API wrapper
     """
 
-    def __init__(self, client_id, client_secret, redirect_uri=None, state=None, cache=None, verify_cert=True, oauth_provider=None):
+    def __init__(self, client_id, client_secret, redirect_uri=None, state=None, cache=None, verify_cert=True, oauth_provider=None, user_id=None):
         # Auth
         if oauth_provider:
-            self.oauth = oauth_provider(client_id, client_secret, redirect_uri, state, cache, verify_cert)
+            self.oauth = oauth_provider(client_id, client_secret, user_id, redirect_uri, state, cache, verify_cert)
         else:
             self.oauth = OAuth(client_id, client_secret, redirect_uri, state, cache, verify_cert)
 
